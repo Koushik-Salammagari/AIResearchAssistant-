a4bc5b4cbf19e229b731472b64ca7232c23a0336
EC2 Default User <ec2-user@ip-172-31-13-158.us-west-1.compute.internal>
2024-07-12 04:34:27 +0000

    small change 1

--- App.py
+++ App.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+import time 
 st.title('AI-Research-Assistance')
 
 st.subheader("Hi I am summer: Your research help Assistant ")
+
+row1 = st.columns(2)
+row2 = st.columns(2)
+
+grid =[col.container(height= 200) for col in row1 + row2]
