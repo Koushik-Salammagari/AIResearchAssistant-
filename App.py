336e73ef3cb591fdd901f422f91a996ce3ab03a7
koushik <koushik.7rdd@gmail.com>
2024-07-13 00:46:05 +0000

    Added About, Home, Account

--- App.py
+++ App.py
@@ -1,9 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from streamlit_option_menu import option_menu
+import About, Account, Home
 
 
 import time 
+
+st.set_page_config(page_title = "Summer")
+
+
 st.title('AI-Research-Assistance')
 
 st.subheader("Hi I am summer: Your research help Assistant ")
@@ -12,4 +18,50 @@ row1 = st.columns(3)
 row2 = st.columns(3)
 
 grid =[col.container(height= 200) for col in row1 + row2]
+
+class MultiApp:
+
+    def __init__(self):
+        self.apps = []
+    
+    def add_app(self, title, function):
+        self.apps.append({
+            "title": title,
+            "function": function
+        })
+    def run():
+        with st.sidebar:
+            app = option_menu(
+                menu_title="SummerAI",
+                options=['Home', 'About', 'Account'],
+                icons=['house-fill', 'person-circle', 'trophy-fill', 'chat-fill', 'info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important", "background-color": 'black'},
+
+                    "icon": {"color": "white", "font-size": "23px"},
+                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "blue"},
+                    "nav-link-selected": {"background-color": "#02ab21"},
+                }
+            )
+
+            if app == 'Home':
+                Home.app()
+            if app == "About":
+                About.app()
+            if app == "Account":
+                Account.app()
+            
+    run()
+
+# if __name__ == "__main__":
+#     run()
+
+    
+                
+
+
+
+
 
---
+++ Home.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+def app():
+    st.write("Back to Home page")
+    #st.set_page_config(page_title="Summer")
+
+    # st.title('AI-Research-Assistance')
+    # st.subheader("Hi I am Summer: Your research help Assistant")
+    
+    # row1 = st.columns(3)
+    # row2 = st.columns(3)
+    
+    # grid = [col.container() for col in row1 + row2]
+
+
+    
+
+
+#st.title('AI-Research-Assistance')
+
+# st.subheader("Hi I am summer: Your research help Assistant ")
+
+# row1 = st.columns(3)
+# row2 = st.columns(3)
+
+# grid =[col.container(height= 200) for col in row1 + row2]
+
